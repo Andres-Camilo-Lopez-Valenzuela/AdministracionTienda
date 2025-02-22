56e33bcd47d77e3114a0e92aec313a141044c899
Andres-Camilo-Lopez-Valenzuela <194439477+Andres-Camilo-Lopez-Valenzuela@users.noreply.github.com>
2025-02-22 08:12:54 -0500

    Añadiendo funciones al menú principal

--- Principal.py
+++ Principal.py
@@ -1,5 +1,19 @@
 # Este es el archivo Principal
 import modulos.ui as ui
 
+# Muestra el menú
 ui.men1()
 
+opcion = None
+
+try:
+    opcion = int(input('Por favor elija una opción: '))
+except ValueError:
+    print('Ingrese un número para continuar porfavor 😓')
+
+if opcion is not None:
+    match opcion:
+        case 1:
+            print('Funcionando de momento')
+        case  _:
+            print('Tambien esta funcionando')
